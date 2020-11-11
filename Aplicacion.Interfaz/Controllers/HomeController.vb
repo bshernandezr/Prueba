a3264f93fbb8c82e92a0aa92960fb6a4ce8ea839
brayan steeven hernandez rodriguez <bshernandezr@unal.edu.co>
2020-11-11 11:58:02 -0500

    Agregar archivos de proyecto.

---
+++ Aplicacion.Interfaz/Controllers/HomeController.vb
@@ -0,0 +1,31 @@
+﻿
+Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Private db As New ModeloConnection
+    'Agregamos el metodo Get para obtener la lista
+    <HttpGet>
+    Function Index() As ActionResult
+        Return View(db.tienda.ToList())
+    End Function
+    'Agregamos el metodo Post para filtrar la lista teniendo como argumento el filtro
+    <HttpPost>
+    Function Index(filtro As String) As ActionResult
+        Dim Tabla = db.tienda
+        Dim Lista = db.tienda.ToList()
+        For Each item In Lista
+            Dim borrar = db.tienda.FirstOrDefault(Function(x) x.NombreEmpleado = item.NombreEmpleado)
+            Tabla.Remove(borrar)
+        Next
+        Dim ListaFiltrada = db.empleado.GroupBy(Function(x) x.Cargo = filtro).ToList()
+        For Each fila In ListaFiltrada
+            For Each empleado In fila
+                Dim dato = db.tienda.FirstOrDefault(Function(x) x.NombreEmpleado = empleado.NombreEmpleado)
+                Tabla.Add(dato)
+            Next
+        Next
+        Return View(Tabla.ToList())
+    End Function
+
+
+End Class
---
+++ Aplicacion.Interfaz/Views/Home/Index.vbhtml
@@ -0,0 +1,35 @@
+﻿@ModelType  IEnumerable(Of tienda)
+@*Definimos el modelo de la vista t creamos una variable string que se enviara al metodo post y hacer el filtro*@
+@code
+    Dim a = " "
+End Code
+
+<h2>Registro de empleados</h2>
+
+<form action="@Url.Action("Index", "Home", New With {.filtro = a})" method="post">     
+    <div class="form-group">
+        <label>Filtrar por cargo</label> <br /> 
+        @Html.TextBox(a)
+        @*<input type="text" name="cargo" class="form-control" />*@ 
+        <input class="btn btn-success" type="submit" value="Filtrar" />
+    </div>
+        
+</form>
+
+<hr />
+
+<table class="table table-bordered">
+    <tr>
+        <th>Dia trabajado</th>
+        <th>Tienda</th>
+        <th>Empleado</th>
+    </tr>
+    @*Recorremos la tabla e imprimimos los empleados segun las especificaciones*@
+    @For Each registro In Model
+    @<tr>
+        <td>@registro.FechaTrabajo</td>
+        <td>@registro.NombreTienda</td>
+        <td>@registro.NombreEmpleado</td>
+    </tr>
+    Next
+</table>
